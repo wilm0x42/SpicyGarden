325d18a013c897c72592124016980d06fe46ec47
Jesse <jesse.toftum@utah.edu>
2022-10-07 13:17:33 -0600

    Quick reformat pass uwu

--- spigot-plugin/SpicyGarden.java
+++ spigot-plugin/SpicyGarden.java
@@ -12,82 +12,72 @@ import org.bukkit.Bukkit;
 //import org.bukkit.event.*;
 //import org.bukkit.event.player.*;
 
+public class SpicyGarden extends JavaPlugin {
+    int spawn_search_distance = 1000; // distance in all directions to search from spawn, in a square
+    int search_point_spacing = 50; // distance between blocks to query for data
+    int search_height = 64; // constant y-value for block queries
 
-public class SpicyGarden extends JavaPlugin
-{
-	int spawn_search_distance = 1000; // distance in all directions to search from spawn, in a square
-	int search_point_spacing = 50; // distance between blocks to query for data
-	int search_height = 64; // constant y-value for block queries
+    @Override
+    public void onEnable() {
+        // getServer().getPluginManager().registerEvents(new SpicyListener(), this);
 
-	@Override
-	public void onEnable()
-	{
-		//getServer().getPluginManager().registerEvents(new SpicyListener(), this);
-		
-		try {
-			Bukkit.getLogger().info("SPICY GARDEN -- LET'S DO THIS");
-			org.bukkit.World world = org.bukkit.Bukkit.getWorld("world");
-			
-			FileWriter data_output = new FileWriter("SpicyGardenData.txt");
-			
-			data_output.write(String.format("Seed: %d\n", world.getSeed()));
-			
-			StructureType[] interesting_structures = {
-				StructureType.DESERT_PYRAMID,
-				StructureType.JUNGLE_PYRAMID,
-				StructureType.OCEAN_MONUMENT,
-				StructureType.VILLAGE,
-				StructureType.WOODLAND_MANSION,
-			};
-			
-			Bukkit.getLogger().info("Yoinking Structures...");
-			
-			for (StructureType structure: interesting_structures)
-			{
-				Location nearest = world.locateNearestStructure(new Location(world, 0.0, search_height, 0.0),
-																structure,
-																spawn_search_distance,
-																false);
-				if (nearest != null)
-				{
-					data_output.write(String.format("Structure: (%d,%d,%d) %s\n",
-													nearest.getBlockX(),
-													nearest.getBlockY(),
-													nearest.getBlockZ(),
-													structure.getName()));
-				}
-				else
-				{
-					data_output.write(String.format("Structure: NOTFOUND %s\n", structure.toString()));
-				}
-			}
-			
-			Bukkit.getLogger().info("Yoinking Biomes...");
-			
-			for (int x = -spawn_search_distance; x < spawn_search_distance; x += search_point_spacing)
-			{
-				for (int z = -spawn_search_distance; z < spawn_search_distance; z += search_point_spacing)
-				{
-					org.bukkit.block.Block block = world.getBlockAt(x, search_height, z);
-					
-					org.bukkit.block.Biome biome = block.getBiome();
-					
-					data_output.write(String.format("Biome: (%d,%d,%d) %s\n", x, search_height, z, biome.toString()));
-				}
-			}
-			
-			Bukkit.getLogger().info("Finishing up...");
-			
-			data_output.close();
-		}
-		catch (IOException e) {}
-		
-		Bukkit.getLogger().info("Shutting down server...");
-		Bukkit.shutdown();
-	}
-	
-	@Override
-	public void onDisable()
-	{
-	}
+        try {
+            Bukkit.getLogger().info("SPICY GARDEN -- LET'S DO THIS");
+            org.bukkit.World world = org.bukkit.Bukkit.getWorld("world");
+
+            FileWriter data_output = new FileWriter("SpicyGardenData.txt");
+
+            data_output.write(String.format("Seed: %d\n", world.getSeed()));
+
+            StructureType[] interesting_structures = {
+                    StructureType.DESERT_PYRAMID,
+                    StructureType.JUNGLE_PYRAMID,
+                    StructureType.OCEAN_MONUMENT,
+                    StructureType.VILLAGE,
+                    StructureType.WOODLAND_MANSION,
+            };
+
+            Bukkit.getLogger().info("Yoinking Structures...");
+
+            for (StructureType structure : interesting_structures) {
+                Location nearest = world.locateNearestStructure(new Location(world, 0.0, search_height, 0.0),
+                        structure,
+                        spawn_search_distance,
+                        false);
+                if (nearest != null) {
+                    data_output.write(String.format("Structure: (%d,%d,%d) %s\n",
+                            nearest.getBlockX(),
+                            nearest.getBlockY(),
+                            nearest.getBlockZ(),
+                            structure.getName()));
+                } else {
+                    data_output.write(String.format("Structure: NOTFOUND %s\n", structure.toString()));
+                }
+            }
+
+            Bukkit.getLogger().info("Yoinking Biomes...");
+
+            for (int x = -spawn_search_distance; x < spawn_search_distance; x += search_point_spacing) {
+                for (int z = -spawn_search_distance; z < spawn_search_distance; z += search_point_spacing) {
+                    org.bukkit.block.Block block = world.getBlockAt(x, search_height, z);
+
+                    org.bukkit.block.Biome biome = block.getBiome();
+
+                    data_output.write(String.format("Biome: (%d,%d,%d) %s\n", x, search_height, z, biome.toString()));
+                }
+            }
+
+            Bukkit.getLogger().info("Finishing up...");
+
+            data_output.close();
+        } catch (IOException e) {
+        }
+
+        Bukkit.getLogger().info("Shutting down server...");
+        Bukkit.shutdown();
+    }
+
+    @Override
+    public void onDisable() {
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::fs;
-use std::thread;
-use std::sync::{mpsc, atomic};
-use std::time::Duration;
 use std::convert::TryFrom;
+use std::fs;
 use std::process::{Command, Stdio};
+use std::sync::{atomic, mpsc};
+use std::thread;
+use std::time::Duration;
 
-use iced::{executor, Application, Button, Column, Row, Element,
-    Settings, Text, TextInput, Padding, Subscription};
+use iced::{
+    executor, Application, Button, Column, Element, Padding, Row, Settings, Subscription, Text,
+    TextInput,
+};
 extern crate iced_native;
 
 extern crate fs_extra;
@@ -26,7 +28,11 @@ struct Seed {
 }
 
 fn run_server(mut target_seed: Seed) -> Seed {
-    println!("Running server {} with seed {}", target_seed.claimed_runner_index.unwrap(), target_seed.seed);
+    println!(
+        "Running server {} with seed {}",
+        target_seed.claimed_runner_index.unwrap(),
+        target_seed.seed
+    );
 
     let runner_index = target_seed.claimed_runner_index.unwrap();
     let runner_dir = format!("runners/runner_{}", runner_index);
@@ -36,9 +42,13 @@ fn run_server(mut target_seed: Seed) -> Seed {
     match fs::create_dir_all(runner_dir.clone()) {
         Ok(_) => (),
         Err(e) => {
-            println!("ERROR: Unable to create directory {} - {:?}", runner_dir.clone(), e);
+            println!(
+                "ERROR: Unable to create directory {} - {:?}",
+                runner_dir.clone(),
+                e
+            );
             return target_seed;
-        },
+        }
     };
 
     // Copy the template into our runner directory
@@ -48,22 +58,26 @@ fn run_server(mut target_seed: Seed) -> Seed {
     match fs_extra::dir::copy("server_template", runner_dir.clone(), &copy_options) {
         Ok(_) => (),
         Err(e) => {
-            println!("ERROR: Unable to copy server_template into {} - {:?}", runner_dir, e);
+            println!(
+                "ERROR: Unable to copy server_template into {} - {:?}",
+                runner_dir, e
+            );
             return target_seed;
-        },
+        }
     };
 
     // Write a seed-specific (and runner-specific) server.properties
-    let server_properties: String = serverproperties::get_server_properties(
-        runner_index,
-        &target_seed.seed.clone()
-    );
-    match fs::write(format!("{}/server.properties", runner_dir.clone()), server_properties) {
+    let server_properties: String =
+        serverproperties::get_server_properties(runner_index, &target_seed.seed.clone());
+    match fs::write(
+        format!("{}/server.properties", runner_dir.clone()),
+        server_properties,
+    ) {
         Ok(_) => (),
         Err(e) => {
             println!("ERROR: Unable to write to server.properties - {:?}", e);
             return target_seed;
-        },
+        }
     };
 
     // Start the java server in a child process
@@ -72,12 +86,13 @@ fn run_server(mut target_seed: Seed) -> Seed {
         .args(["-Xms32M", "-Xmx512M", "-jar", "server.jar", "nogui"])
         .stdin(Stdio::null())
         .stdout(Stdio::null())
-        .spawn() {
+        .spawn()
+    {
         Ok(process) => process,
         Err(e) => {
             println!("ERROR: Unable to start minecraft server: {:?}", e);
             return target_seed;
-        },
+        }
     };
 
     // Elaborate busy loop because rust doesn't help you timeout child processes
@@ -94,7 +109,7 @@ fn run_server(mut target_seed: Seed) -> Seed {
                 Ok(()) => {
                     server_process.wait().unwrap(); // Wait to ensure resources are released
                 }
-                Err(_) => () // Process already died
+                Err(_) => (), // Process already died
             };
             return target_seed;
         }
@@ -107,45 +122,54 @@ fn run_server(mut target_seed: Seed) -> Seed {
                     println!("ERROR: Minecraft server exited with failure: {}", status);
                     return target_seed;
                 }
-            },
+            }
             Ok(None) => {
                 thread::sleep(Duration::from_millis(10));
-            },
+            }
             Err(e) => {
                 println!("ERROR: Failed while waiting on java process: {:?}", e);
                 return target_seed;
-            },
+            }
         }
 
         match timeout_rx.try_recv() {
             Ok(_timeout) => {
-                println!("TIMEOUT: Runner {} exceeded timeout, giving up", runner_index);
+                println!(
+                    "TIMEOUT: Runner {} exceeded timeout, giving up",
+                    runner_index
+                );
                 match server_process.kill() {
                     Ok(()) => {
                         server_process.wait().unwrap(); // Wait to ensure resources are released
                     }
-                    Err(_) => () // Process already died
+                    Err(_) => (), // Process already died
                 };
                 return target_seed;
-            },
+            }
             Err(_e) => (),
         }
     }
- 
+
     // Check for results
-    
+
     let server_result = match fs::read(format!("{}/SpicyGardenData.txt", runner_dir)) {
         Ok(result_txt) => result_txt,
         Err(e) => {
-            println!("ERROR: Unable to read SpicyGardenData.txt on runner {}: {:?}", runner_index, e);
+            println!(
+                "ERROR: Unable to read SpicyGardenData.txt on runner {}: {:?}",
+                runner_index, e
+            );
             return target_seed;
-        },
+        }
     };
 
     let decoded_server_result = match String::from_utf8(server_result) {
         Ok(decoded) => decoded,
         Err(e) => {
-            println!("ERROR: Failed to decode SpicyGardenData.txt on runner {}: {:?}", runner_index, e);
+            println!(
+                "ERROR: Failed to decode SpicyGardenData.txt on runner {}: {:?}",
+                runner_index, e
+            );
             return target_seed;
         }
     };
@@ -175,42 +199,44 @@ fn seed_search_loop(gather_server_address: String, client_key: String, target_ru
                 let received = rx.recv().unwrap_or_else(|error| {
                     panic!("Thread communication error: {:?}", error);
                 });
-        
+
                 halted_runners.push(received.claimed_runner_index.unwrap());
             }
             break;
         }
-        
+
         // Make sure we've got seeds from the gather server in the pool
 
         let seed_pool_count: u32 = u32::try_from(seed_pool.len()).unwrap();
 
         if seed_pool_count < target_runner_count {
             let requested_seed_count = target_runner_count - seed_pool_count;
-            let request_uri = format!("{}/assign_seeds/{}/{}",
-                gather_server_address,
-                client_key,
-                requested_seed_count,
+            let request_uri = format!(
+                "{}/assign_seeds/{}/{}",
+                gather_server_address, client_key, requested_seed_count,
             );
-            
+
             let response = match http_client.get(request_uri).send() {
                 Ok(r) => r,
                 Err(e) => {
                     println!("Unable to contact seed server: {:?} Retrying in 3s.", e);
                     thread::sleep(Duration::from_secs(3));
                     continue;
-                },
+                }
             };
 
             if response.status() != 200 {
-                println!("Bad response from seed server: {:?} Retrying in 3s.", response.status());
+                println!(
+                    "Bad response from seed server: {:?} Retrying in 3s.",
+                    response.status()
+                );
                 thread::sleep(Duration::from_secs(3));
                 continue;
             }
-                
+
             let body_text = response.text().unwrap();
             let assigned_seeds: Vec<&str> = body_text.split("\n").collect();
-            
+
             for seed in assigned_seeds {
                 let new_seed = Seed {
                     seed: seed.to_string(),
@@ -241,31 +267,30 @@ fn seed_search_loop(gather_server_address: String, client_key: String, target_ru
         while completed_seeds.len() > 0 {
             let seed = completed_seeds.pop().unwrap();
 
-            let request_uri = format!("{}/submit_result/{}",
-                gather_server_address,
-                client_key,
-            );
+            let request_uri = format!("{}/submit_result/{}", gather_server_address, client_key,);
 
-            let _response = match http_client.post(request_uri)
+            let _response = match http_client
+                .post(request_uri)
                 .header("SpicyGarden-Seed", seed.seed.clone())
                 .body(seed.result.clone().unwrap())
-                .send() {
-                    Ok(r) => {
-                        if r.status() != 204 {
-                            println!("Error submitting to seed server: {:?}", r.status());
-                            completed_seeds.push(seed);
-                            break;
-                        }
-                        println!("Sent result for seed: {:?}", seed.seed.clone());
-                    },
-                    Err(e) => {
-                        println!("Unable to submit to seed server: {:?}", e);
+                .send()
+            {
+                Ok(r) => {
+                    if r.status() != 204 {
+                        println!("Error submitting to seed server: {:?}", r.status());
                         completed_seeds.push(seed);
                         break;
-                    },
-                };
+                    }
+                    println!("Sent result for seed: {:?}", seed.seed.clone());
+                }
+                Err(e) => {
+                    println!("Unable to submit to seed server: {:?}", e);
+                    completed_seeds.push(seed);
+                    break;
+                }
+            };
         }
-        
+
         // Check for completed runners
 
         let received = rx.recv().unwrap_or_else(|error| {
@@ -277,7 +302,7 @@ fn seed_search_loop(gather_server_address: String, client_key: String, target_ru
         match &received.result {
             Some(_result) => {
                 println!("Seed {:?} completed successfully.", received.seed);
-            },
+            }
             None => {
                 println!("Seed {:?} failed to capture data. Skipping.", received.seed);
                 continue;
@@ -304,7 +329,7 @@ enum RunningState {
 
 struct SpicyGarden {
     start_button: iced::button::State,
-    
+
     server_address_input: iced::text_input::State,
     server_address: String,
 
@@ -351,7 +376,7 @@ impl Application for SpicyGarden {
                 status_message: "".to_string(),
                 running_state: RunningState::Waiting,
             },
-            iced::Command::none()
+            iced::Command::none(),
         )
     }
 
@@ -360,67 +385,72 @@ impl Application for SpicyGarden {
     }
 
     fn subscription(&self) -> Subscription<Message> {
-        iced_native::subscription::events().map(|event| {
-            match event {
-                iced_native::Event::Window(window_event) => {
-                    match window_event {
-                        iced_native::window::Event::CloseRequested => Message::Quit,
-                        _ => Message::IgnorableEvent
-                    }
-                }
-                _ => Message::IgnorableEvent
-            }
+        iced_native::subscription::events().map(|event| match event {
+            iced_native::Event::Window(window_event) => match window_event {
+                iced_native::window::Event::CloseRequested => Message::Quit,
+                _ => Message::IgnorableEvent,
+            },
+            _ => Message::IgnorableEvent,
         })
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        let mut column = Column::new()
-            .push(Row::new()
-                .push(Text::new("SpicyGarden by wilm0x42").size(32))
-            );
-        
+        let mut column =
+            Column::new().push(Row::new().push(Text::new("SpicyGarden by wilm0x42").size(32)));
+
         if self.running_state == RunningState::Waiting {
-            column = column.push(Row::new()
-                .push(Text::new("Server address:"))
-                .push(TextInput::new(
-                    &mut self.server_address_input,
-                    "example.com",
-                    &self.server_address,
-                    Message::ServerAddressChanged,
-                    ).padding(Padding::from(8))
+            column = column
+                .push(
+                    Row::new()
+                        .push(Text::new("Server address:"))
+                        .push(
+                            TextInput::new(
+                                &mut self.server_address_input,
+                                "example.com",
+                                &self.server_address,
+                                Message::ServerAddressChanged,
+                            )
+                            .padding(Padding::from(8)),
+                        )
+                        .align_items(iced::Alignment::Center)
+                        .spacing(8),
                 )
-                .align_items(iced::Alignment::Center)
-                .spacing(8)
-            )
-            .push(Row::new()
-                .push(Text::new("Client key:"))
-                .push(TextInput::new(
-                    &mut self.client_key_input,
-                    "super_secret_key",
-                    &self.client_key,
-                    Message::ClientKeyChanged,
-                    ).padding(Padding::from(8))
+                .push(
+                    Row::new()
+                        .push(Text::new("Client key:"))
+                        .push(
+                            TextInput::new(
+                                &mut self.client_key_input,
+                                "super_secret_key",
+                                &self.client_key,
+                                Message::ClientKeyChanged,
+                            )
+                            .padding(Padding::from(8)),
+                        )
+                        .align_items(iced::Alignment::Center)
+                        .spacing(8),
                 )
-                .align_items(iced::Alignment::Center)
-                .spacing(8)
-            )
-            .push(Row::new()
-                .push(Text::new("Concurrent server count:"))
-                .push(TextInput::new(
-                    &mut self.runner_count_input,
-                    "4",
-                    &self.runner_count,
-                    Message::RunnerCountChanged,
-                    ).padding(Padding::from(8))
+                .push(
+                    Row::new()
+                        .push(Text::new("Concurrent server count:"))
+                        .push(
+                            TextInput::new(
+                                &mut self.runner_count_input,
+                                "4",
+                                &self.runner_count,
+                                Message::RunnerCountChanged,
+                            )
+                            .padding(Padding::from(8)),
+                        )
+                        .align_items(iced::Alignment::Center)
+                        .spacing(8),
                 )
-                .align_items(iced::Alignment::Center)
-                .spacing(8)
-            )
-            .push(Button::new(&mut self.start_button, Text::new("Start gathering data"))
-                .on_press(Message::StartSeedSearch)
-                .padding(Padding::from(16))
-            )
-            .push(Text::new(self.status_message.clone()))
+                .push(
+                    Button::new(&mut self.start_button, Text::new("Start gathering data"))
+                        .on_press(Message::StartSeedSearch)
+                        .padding(Padding::from(16)),
+                )
+                .push(Text::new(self.status_message.clone()))
         };
 
         if self.running_state == RunningState::Running {
@@ -459,30 +489,30 @@ impl Application for SpicyGarden {
 
                 return iced::Command::perform(
                     seed_search_async_wrapper(server_address, client_key, runner_count),
-                    |_| {Message::StoppedSeedSearch}
+                    |_| Message::StoppedSeedSearch,
                 );
-            },
+            }
             Message::ServerAddressChanged(value) => {
                 self.server_address = value;
-            },
+            }
             Message::ClientKeyChanged(value) => {
                 self.client_key = value;
-            },
+            }
             Message::RunnerCountChanged(value) => {
                 self.runner_count = value;
-            },
+            }
             Message::StoppedSeedSearch => {
                 self.running_state = RunningState::Quit;
-            },
+            }
             Message::Quit => {
                 if self.running_state == RunningState::Running {
                     self.running_state = RunningState::Quitting;
                     JAVA_THREADS_SHUTDOWN.store(true, atomic::Ordering::Relaxed);
                 } else {
                     self.running_state = RunningState::Quit;
-                }; 
-            },
-            Message::IgnorableEvent => {},
+                };
+            }
+            Message::IgnorableEvent => {}
         }
         iced::Command::none()
     }
